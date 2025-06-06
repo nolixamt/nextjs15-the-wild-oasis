c83c13641035824ffb2a9410dfb1320d3a6faf00
Arnor Ameti <arnor.ameti@coop.ch>
2025-06-06 17:35:50 +0200

    feat: defining layout of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import React from 'react';
+import React from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen bg-primary-900 text-accent-50 grid grid-rows-[auto_1fr]`}
       >
-        {children}
+        <header className={"px-8 py-5 text-xl font-semibold"}>
+          <div className={"max-w-7xl mx-auto"}>The Wild Oasis</div>
+        </header>
+
+        <div className={"px-8 py-12"}>
+          <main className={"max-w-7xl mx-auto"}>{children}</main>
+        </div>
       </body>
     </html>
   );
